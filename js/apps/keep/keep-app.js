09a37366838e42dcc85c3e6766594c37e3062c1f
Ilya <khirsah2211@gmail.com>
2021-11-10 11:35:26 +0200

    utils

--- js/apps/keep/keep-app.js
+++ js/apps/keep/keep-app.js
@@ -1,5 +1,30 @@
+import { noteService } from './services/note.service.js';
+import keepPreview from './cmps/keep-preview.cmp.js';
+
 export default {
+  components: {
+    keepPreview,
+  },
   template: `
-    <h1>Hi keep</h1>
+    <section class="keep-app">
+      <keep-preview :notes="notes"/>
+    </section>
     `,
+  data() {
+    return {
+      notes: null,
+    };
+  },
+  created() {
+    this.loadNotes();
+  },
+  methods: {
+    loadNotes() {
+      console.log(noteService.query());
+      noteService.query().then((notes) => {
+        this.notes = notes;
+        console.log(this.notes);
+      });
+    },
+  },
 };
