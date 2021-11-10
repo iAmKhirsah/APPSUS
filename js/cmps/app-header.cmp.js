2ee2ddeaa55eac08ed7d253ec89a9dfa05bb9eed
Ilya <khirsah2211@gmail.com>
2021-11-10 10:40:11 +0200

    initial css setup

---
+++ js/cmps/app-header.cmp.js
@@ -0,0 +1,12 @@
+export default {
+  template: `
+  <section class="main-header">
+      <h1>Appsus</h1>
+    <nav>
+        <router-link to="/">Home </router-link>
+        <router-link to="/mail">Mail </router-link>
+        <router-link to="/keep">Keep </router-link>
+    </nav>
+  </section>
+    `,
+};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import keepApp from './apps/keep/keep-app.js';
 import mailApp from './apps/mail/mail-app.js';
 import homePage from './pages/app-home.cmp.js';
+import appHeader from './cmps/app-header.cmp.js';
 import { router } from './routes.js';
 
 const options = {
@@ -10,9 +11,11 @@ const options = {
     keepApp,
     mailApp,
     homePage,
+    appHeader,
   },
   template: `
   <section>
+    <app-header />
     <home-page />
     <!-- <keep-app /> -->
     <router-view />
