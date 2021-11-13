2eaaa444a865ab5e1dc6278d2da73eab0d846674
Ilya <khirsah2211@gmail.com>
2021-11-13 17:52:01 +0200

    ?

--- js/apps/keep/keep-app.js
+++ js/apps/keep/keep-app.js
@@ -32,11 +32,10 @@ export default {
   methods: {
     sendMail(note) {
       let id = note.id
-      let txt = note.info.txt
       eventBus.$emit('noteToMail', note.info);
       this.$nextTick(() => {
         // this.$router.push('/mail/noteToMail');
-        this.$router.push({path:'mail', query: {txt: txt}});
+        this.$router.push({path:'mail', query: {id: id}});
       });
     },
     duplicate(note) {
